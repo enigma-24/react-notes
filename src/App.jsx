e6d1f7e3c82b3182474ce9d9c5da20a69a53afb2
aniket <50738821+enigma-24@users.noreply.github.com>
2025-02-15 16:49:45 -0500

    add notification component for showing message when there is any error

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import { useState, useEffect } from 'react';
 import noteService from './services/notes';
 import Note from './components/Note';
+import Notification from './components/Notification';
 
 const App = () => {
 	const [notes, setNotes] = useState([]);
 	const [newNote, setNewNote] = useState('');
 	const [showAll, setShowAll] = useState(true);
+	const [errorMessage, setErrorMessage] = useState('some error happened');
 
 	useEffect(() => {
 		noteService.getAll().then((notes) => setNotes(notes));
@@ -39,7 +41,8 @@ const App = () => {
 				setNotes(notes.map((note) => (note.id === id ? modifiedNote : note)))
 			)
 			.catch((error) => {
-				alert(`the note ${note.content} was already deleted from server`);
+				setErrorMessage(`Note ${note.content} was already removed from server`);
+				setTimeout(() => setErrorMessage(null), 5000);
 				setNotes(notes.filter((note) => note.id !== id));
 			});
 	};
@@ -49,6 +52,7 @@ const App = () => {
 	return (
 		<div>
 			<h1>Notes</h1>
+			<Notification message={errorMessage} />
 			<div>
 				<button onClick={() => setShowAll(!showAll)}>
 					show {showAll ? 'important' : 'all'}
